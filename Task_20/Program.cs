9eab32483d185a3ef1e8d3f0981c3a588be346b3
Olesya <lesusha1@yandex.ru>
2022-09-10 13:02:22 +0300

    add Task_20

--- Task_20/Program.cs
+++ Task_20/Program.cs
@@ -4,14 +4,16 @@
 // A (7,-5); B (1,-1) → 7,21
 
 Console.WriteLine("Введите координаты X Y точки a:");
-string XYa = Console.ReadLine()!;  
+string XYa = Console.ReadLine()!;
 string[] parts1 = XYa.Split(' ');
 int Xa = int.Parse(parts1[0]);
-int Ya = int.Parse(parts1[1]); 
+int Ya = int.Parse(parts1[1]);
 
 Console.WriteLine("Введите координаты X Y точки b:");
-string XYb = Console.ReadLine()!;  
+string XYb = Console.ReadLine()!;
 string[] parts2 = XYb.Split(' ');
 int Xb = int.Parse(parts2[0]);
-int Yb = int.Parse(parts2[1]); 
+int Yb = int.Parse(parts2[1]);
 
+double distanseAB = Math.Sqrt((Xb - Xa) * (Xb - Xa) + (Yb - Ya) * (Yb - Ya));
+Console.WriteLine(distanseAB - distanseAB % 0.01);
